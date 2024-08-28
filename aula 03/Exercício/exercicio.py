c57fccd48552d5d9f0eee86e09e3cccec6152ea1
Gustavo Ferreira <gfmb94@gmail.com>
2024-08-28 20:52:05 -0300

    correção exercício

---
+++ aula 03/Exercício/exercicio.py
@@ -0,0 +1,17 @@
+legumes = [
+    "Alface",
+    "Tomate",
+    "Maçã",
+    "Limão",
+    "Laranja",
+    "Morango",
+    "Cenoura",
+    "Cebola",
+    "Chuchu",
+    "Melância"
+]
+
+y = 0
+for x in legumes:
+    print(y,"-",x)
+    y+=1
