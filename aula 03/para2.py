6bd56a6b5aaadc2c02ccda28d8db868a1e6c15f3
Gustavo Ferreira <gfmb94@gmail.com>
2024-08-28 18:56:14 -0300

    Laços de repetição

---
+++ aula 03/para2.py
@@ -0,0 +1,14 @@
+alunos = [
+    "Gustavo",
+    "Binca",
+    "Matheus",
+    "Bruno",
+    "Ailton"
+]
+alunos.sort()
+alunos.append("Vitoria")
+alunos.reverse()
+y = 1
+for x in alunos:
+    print(y,"-",x)
+    y = y + 1
